d0c613138dd1985d8a80a38f4cf1cc5034af7865
AnNgoexe <quocan2004tp@gmail.com>
2024-10-28 01:12:54 +0700

    fix routes

--- Server/routes/verifyRouter.js
+++ Server/routes/verifyRouter.js
@@ -5,10 +5,10 @@ const VerifyController = require('../controllers/VerifyController');
 const validateAccount = require('../middlewares/validateResetAccount');
 
 // Route to resend OTP for email verification
-verifyRouter.post('/resend-mail-otp-verification', validateAccount, VerifyController.resendMailOTPVerification);
+verifyRouter.post('/resend-otp-verification', validateAccount, VerifyController.resendOTPVerification);
 
 // Route to resend OTP for account recovery via email
-verifyRouter.post('/resend-mail-otp-recovery', validateAccount, VerifyController.resendMailOTPRecovery);
+verifyRouter.post('/resend-otp-recovery', validateAccount, VerifyController.resendOTPRecovery);
 
 // Route to verify account using email or phone number with OTP
 verifyRouter.post('/verify-account', validateAccount, VerifyController.verifyAccount);
