9edede0bb4ba0ff95e4750a051ed9b4ba65ae25e
AnNgoexe <quocan2004tp@gmail.com>
2024-10-28 01:17:00 +0700

    add dependencies

--- Mobile/index.js
+++ Mobile/index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import SignupForm from './src/components/forms/signup-form/SignupForm';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => SignupForm);
