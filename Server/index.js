806cd76f49c8eaab5c48a356f6e7474ad3d9f408
AnNgoexe <quocan2004tp@gmail.com>
2024-11-03 16:25:42 +0700

    update server

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+const IP = process.env.IP
 
 app.use(express.json());
+app.use(cors());
 
 const authRouter = require('./routes/authRouter');
 const verifyRouter = require('./routes/verifyRouter');
@@ -12,5 +16,5 @@ app.use('/verify', verifyRouter);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://${IP}:${PORT}`);
 });
